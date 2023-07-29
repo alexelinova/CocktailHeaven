115f952175be26cc76a4dbb410e3bb14ec67aed6
Alex <81240373+alexelinova@users.noreply.github.com>
2023-07-29 19:44:51 +0300

    Added js logic to dynamically add more ingredients - Edit view

---
+++ CocktailHeaven/CocktailHeaven/wwwroot/js/more-ingredients.js
@@ -0,0 +1,60 @@
+﻿const addIngredientBtn = document.getElementById("addIngredientBtn");
+const ingredientContainer = document.getElementById("ingredientsContainer")
+
+let ingredientIndex = ingredientContainer.childElementCount;
+
+addIngredientBtn.addEventListener('click', function () {
+
+	const nextIndex = ingredientIndex++;
+
+	const div = document.createElement("div");
+	div.classList.add("row");
+
+	const ingredientNameDiv = document.createElement("div");
+	ingredientNameDiv.classList.add("mb-3", "col-sm-4");
+
+	const ingredientNameLabel = document.createElement("label");
+	ingredientNameLabel.setAttribute("for", `Ingredients_${nextIndex}__IngredientName`);
+	ingredientNameLabel.textContent = "Ingredient";
+	ingredientNameDiv.appendChild(ingredientNameLabel);
+
+	const ingredientNameInput = document.createElement("input");
+	ingredientNameInput.setAttribute("type", "text");
+	ingredientNameInput.setAttribute("name", `Ingredients[${nextIndex}].IngredientName`);
+	ingredientNameInput.classList.add("form-control");
+	ingredientNameDiv.appendChild(ingredientNameInput);
+
+	const quantityDiv = document.createElement("div");
+	quantityDiv.classList.add("mb-3", "col-sm-4");
+
+	const quantityLabel = document.createElement("label");
+	quantityLabel.setAttribute("for", `Ingredients_${nextIndex}__Quantity`);
+	quantityLabel.textContent = "Quantity";
+	quantityDiv.appendChild(quantityLabel);
+
+	const quantityInput = document.createElement("input");
+	quantityInput.setAttribute("type", "text");
+	quantityInput.setAttribute("name", `Ingredients[${nextIndex}].Quantity`);
+	quantityInput.classList.add("form-control");
+	quantityDiv.appendChild(quantityInput);
+
+	const noteDiv = document.createElement("div");
+	noteDiv.classList.add("mb-3", "col-sm-4");
+
+	const noteLabel = document.createElement("label");
+	noteLabel.setAttribute("for", `Ingredients_${nextIndex}__Note`);
+	noteLabel.innerText = "Note";
+	noteDiv.appendChild(noteLabel);
+
+	const noteInput = document.createElement("input");
+	noteInput.setAttribute("type", "text");
+	noteInput.setAttribute("name", `Ingredients[${nextIndex}].Note`);
+	noteInput.setAttribute("placeholder", "optional");
+	noteInput.classList.add("form-control");
+	noteDiv.appendChild(noteInput);
+
+	div.appendChild(ingredientNameDiv);
+	div.appendChild(quantityDiv);
+	div.appendChild(noteDiv);
+	ingredientsContainer.appendChild(div);
+});
