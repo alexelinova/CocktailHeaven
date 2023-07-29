2102e5b9b00aaf66b6d255c5bab4e68536054cc2
Alex <81240373+alexelinova@users.noreply.github.com>
2023-07-30 00:03:12 +0300

    Added Search Controller

---
+++ CocktailHeaven/CocktailHeaven/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CocktailHeaven.Controllers
+{
+	public class SearchController : BaseController
+	{
+		public  IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
