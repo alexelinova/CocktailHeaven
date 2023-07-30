01e700fa25c7062cf72856a418973c7f0affe476
Alex <81240373+alexelinova@users.noreply.github.com>
2023-07-30 22:53:39 +0300

    Added RatingHelper class for star rating

---
+++ CocktailHeaven/CocktailHeaven/Helpers/RatingHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace CocktailHeaven.Helpers
+{
+	public static class RatingHelper
+	{
+		public static string GetStarClass(int starNumber, double averageRating)
+		{
+			double roundedRating = Math.Floor(averageRating);
+			double decimalPart = averageRating - roundedRating;
+
+			if (starNumber <= roundedRating)
+			{
+				return "active";
+			}
+			else if (starNumber == roundedRating + 1 && decimalPart >= 0.5)
+			{
+				return "half";
+			}
+			else
+			{
+				return "";
+			}
+		}
+	}
+}
